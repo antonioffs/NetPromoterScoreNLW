615481dd0569b16cf6716104019a42c1b12c2965
antonioffs <antonio.ffsmail@gmail.com>
2021-02-25 06:25:47 -0300

    dia 2

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,12 @@
 import 'reflect-metadata';
 import express, { request, response } from 'express';
 import "./database";
+import { router } from './routes';
 
 const app = express();
 
-
-app.get("/", (request, response) => {
-
-    return response.json({ message: "Hello World - NLW 04" });
-
-})
-
-app.post("/", (request, response) =>{
-
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-
-})
+app.use(express.json());
+app.use(router);
 
 
-app.listen(3333, () => console.log("Server is running!"))
+app.listen(3333, () => console.log("Server is running!"));
